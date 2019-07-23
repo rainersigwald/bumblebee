7dcb882f86ba4677135dfda68ae10200f10fe513
Rainer Sigwald <rainer@sigwald.org>
2019-07-23 07:56:10 -0500

    Protoype start for loading an MSBuildWorkspace and getting trees from it

--- bumblebee/Program.cs
+++ bumblebee/Program.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace bumblebee
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            MSBuildLocator.RegisterDefaults();
+
             Console.WriteLine("Hello World!");
+
+            await RunAsync();
+        }
+
+        private static async Task RunAsync()
+        {
+            var workspace = MSBuildWorkspace.Create();
+            var solution = await workspace.OpenSolutionAsync(@"S:\bumblebee\Bumblebee.sln");
+
+            var p = solution.Projects.First();
+
+            var c = await p.GetCompilationAsync();
+
+            //var s = c.SyntaxTrees.First();
+
+            //var r = await s.GetRootAsync();
+
+            //foreach (Microsoft.CodeAnalysis.SyntaxNode n in r.DescendantNodes())
+            //{
+            //    Console.WriteLine($"{n.GetType()}: {n}");
+            //}
+
+            foreach (var syntaxTree in c.SyntaxTrees)
+            {
+                var rewriter = new Rewriter();
+
+                var newSource = rewriter.Visit(syntaxTree.GetRoot());
+            }
         }
     }
 }
