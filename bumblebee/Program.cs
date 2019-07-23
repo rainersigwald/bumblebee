cab144db6883f5fd5787185fd978336a0c2c9f8d
Rainer Sigwald <rainer@sigwald.org>
2019-07-23 15:12:06 -0500

    Simple CLI using DragonFruit

--- bumblebee/Program.cs
+++ bumblebee/Program.cs
@@ -8,19 +8,24 @@ namespace bumblebee
 {
     class Program
     {
-        static async Task Main(string[] args)
+        /// <summary>
+        /// Command-line C# refactoring tool.
+        /// </summary>
+        /// <param name="from">Expression to be replaced.</param>
+        /// <param name="to">Expression to replace <paramref name="from"/> with.</param>
+        /// <param name="solutionPath">Path to a solution; replacement occurs for all compilation units in the solution.</param>
+        /// <returns></returns>
+        static async Task Main(string from, string to, string? solutionPath = null)
         {
             MSBuildLocator.RegisterDefaults();
 
-            Console.WriteLine("Hello World!");
-
-            await RunAsync();
+            await RunAsync(solutionPath, from, to);
         }
 
-        private static async Task RunAsync()
+        private static async Task RunAsync(string? solutionPath, string from, string to)
         {
             var workspace = MSBuildWorkspace.Create();
-            var solution = await workspace.OpenSolutionAsync(@"S:\bumblebee\Bumblebee.sln");
+            var solution = await workspace.OpenSolutionAsync(solutionPath);
 
             var p = solution.Projects.First();
 
