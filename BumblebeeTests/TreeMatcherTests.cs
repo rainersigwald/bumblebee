bb8fbe5753b598099629ca613d643ea5bc8587c0
Rainer Sigwald <raines@microsoft.com>
2019-07-23 09:13:02 -0500

    guess at TreeMatcher shape/test

---
+++ BumblebeeTests/TreeMatcherTests.cs
@@ -0,0 +1,18 @@
+using Bumblebee;
+using FluentAssertions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using Xunit;
+
+namespace BumblebeeTests
+{
+    public class TreeMatcherTests
+    {
+        [Fact]
+        public static void BareIdentifierCanMatch()
+        {
+            
+        }
+    }
+}
---
+++ bumblebee/TreeMatcher.cs
@@ -0,0 +1,17 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Bumblebee
+{
+    public class TreeMatcher
+    {
+        public static SyntaxNode? Match(SyntaxNode tree, Snippet snippet)
+        {
+            return null;
+        }
+    }
+}
